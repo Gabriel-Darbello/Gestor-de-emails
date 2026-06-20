65f99d9ce2d431d141669e6077235b1bd8104317
Gabriel Darbello de Oliveira <gadarbello@gmail.com>
2026-06-19 21:47:00 -0300

    refactor: mudança de nome de EmailClassifier para CLassificationResult

--- src/models/result.py
+++ src/models/result.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from typing import Literal
 
-class ClassifierResult(BaseModel):
+class ClassificationResult(BaseModel):
     category: Literal[
         "DUVIDA",
         "BUG",
